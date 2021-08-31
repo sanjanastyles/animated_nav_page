df1cfb82baba5514b4128e0f9b852ae793479063
KANATA <79627075+sanjanastyles@users.noreply.github.com>
2021-08-31 18:40:07 +0530

    Create home.js

---
+++ home.js
@@ -0,0 +1,12 @@
+<!--hamurber js -->
+  <script>
+    function myFunction() {
+      var x = document.getElementById("mycontent");
+      if (x.className === "content") {
+        x.className += " responsive";
+      } 
+      else {
+        x.className = "content";
+      }
+    }
+  </script>
